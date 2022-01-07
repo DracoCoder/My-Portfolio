e05c565830e5938a2afb6df8c12d80ffdc975980
Gaurav Bhandarkar <62330601+DracoCoder@users.noreply.github.com>
2022-01-08 00:33:03 +0530

    Update ProjectsStyles.js

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const Img = styled.img`
-  width:50%;
+  width:60%;
   height:50%;
   object-fit: cover;
   overflow: hidden;
-  padding-top: 2rem;
+  margin-top: 2rem;
   border-radius: 15px;
 `
 
