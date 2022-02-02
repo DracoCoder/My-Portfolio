bbd365db014b5864d2b97184fcf3253007af4e14
DracoCoder <gauravbhandarkar94@gmail.com>
2022-02-02 12:56:38 +0530

    ..

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -20,8 +20,8 @@ export const projects = [
   // {
   //   title: 'WebRTC App',
   //   description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-  //     image: '/images/3.jpg',
-  //     tags: ['React', 'WebRTC'],
+  //   image: '/images/3.jpg',
+  //   tags: ['React', 'WebRTC'],
   //   source: 'https://google.com',
   //   visit: 'https://google.com',
   //   id: 2,
